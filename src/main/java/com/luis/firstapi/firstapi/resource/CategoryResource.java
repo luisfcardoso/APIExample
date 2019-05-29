1266d4529df71562fd77dab1d80ff7d50adce33a
Luis Filipe Cardoso <lfcamposcardoso@gmail.com>
2019-05-28 23:51:26 -0300

    fix category resource file

--- src/main/java/com/luis/firstapi/firstapi/resource/CategoryResource.java
+++ src/main/java/com/luis/firstapi/firstapi/resource/CategoryResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.luis.firstapi.firstapi.model.Category;
 import com.luis.firstapi.firstapi.repository.CategoryRepository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/categories")
 public class CategoryResource {
 
+    @Autowired
     private CategoryRepository categoryRepository;
 
     @GetMapping
