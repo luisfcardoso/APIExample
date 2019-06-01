3ae790352d043947d9dbf34d3c0dd340b9b2de08
Luis Filipe Cardoso <lfcamposcardoso@gmail.com>
2019-06-01 20:10:05 -0300

    update api exception handler

--- src/main/java/com/luis/firstapi/firstapi/exceptionhandler/APIExceptionHandler.java
+++ src/main/java/com/luis/firstapi/firstapi/exceptionhandler/APIExceptionHandler.java
@@ -4,9 +4,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class APIExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
