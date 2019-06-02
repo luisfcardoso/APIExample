fe5115e5e7912a09f075c7e7fe1b0fd55a7901f0
Luis Filipe Cardoso <lfcamposcardoso@gmail.com>
2019-06-02 13:13:03 -0300

    improve user and dev messages when we use delete method

--- src/main/java/com/luis/firstapi/firstapi/exceptionhandler/APIExceptionHandler.java
+++ src/main/java/com/luis/firstapi/firstapi/exceptionhandler/APIExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContext;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -24,13 +27,14 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Autowired
     private MessageSource messageSource;
+    private RuntimeException ex;
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
-        
-        String userMessage = messageSource.getMessage("invalid.message", null, LocaleContextHolder.getLocale());   
-		String devMessage = ex.getCause().toString();
+
+        String userMessage = messageSource.getMessage("invalid.message", null, LocaleContextHolder.getLocale());
+        String devMessage = ex.getCause().toString();
         List<Error> errors = Arrays.asList(new Error(userMessage, devMessage));
 
         return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
@@ -40,11 +44,19 @@ public class APIExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
-        
+
         List<Error> errors = createErrorList(ex.getBindingResult());
 
         return handleExceptionInternal(ex, errors, headers, HttpStatus.BAD_REQUEST, request);
     }
+
+    @ExceptionHandler({ EmptyResultDataAccessException.class })
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex, WebRequest request) {
+		String userMessage = messageSource.getMessage("resource.not-found", null, LocaleContextHolder.getLocale());
+		String devMessage = ex.toString();
+		List<Error> erros = Arrays.asList(new Error(userMessage, devMessage));
+		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+	}
 
     private List<Error> createErrorList(BindingResult bindingResult) {
         
