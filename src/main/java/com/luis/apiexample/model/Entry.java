93b469ae037fb014b705cccb531ecf1561915dd6
Luis Filipe Cardoso <lfcamposcardoso@gmail.com>
2019-06-09 12:55:37 -0300

    handle errors in entry methods

--- src/main/java/com/luis/apiexample/model/Entry.java
+++ src/main/java/com/luis/apiexample/model/Entry.java
@@ -14,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "entry")
@@ -22,20 +23,26 @@ public class Entry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    @NotNull
     private String description;
 
+    @NotNull
     @Column(name = "due_date")
     private LocalDate dueDate; 
 
     @Column(name = "payment_date")    
     private LocalDate paymentDate;
 
+    @NotNull
     private BigDecimal amount; 
     private String note;
     
+    @NotNull
     @Enumerated(EnumType.STRING)
     private EntryType type;
 
+    @NotNull
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category categoryId;
