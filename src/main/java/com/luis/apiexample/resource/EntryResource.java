0cc1758026b74b57eeb805d7e90893424271c336
Luis Filipe Cardoso <lfcamposcardoso@gmail.com>
2019-06-09 00:42:02 -0300

    upload entry methods

--- src/main/java/com/luis/apiexample/resource/EntryResource.java
+++ src/main/java/com/luis/apiexample/resource/EntryResource.java
@@ -2,13 +2,21 @@ package com.luis.apiexample.resource;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import com.luis.apiexample.event.CreatedResourceEvent;
 import com.luis.apiexample.model.Entry;
 import com.luis.apiexample.repository.EntryRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +25,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class EntryResource {
 
 	@Autowired
-	private EntryRepository entryRepository;
+    private EntryRepository entryRepository;
+    
+    @Autowired
+	private ApplicationEventPublisher publisher;
 	
 	@GetMapping
 	public List<Entry> list() {
@@ -28,6 +39,13 @@ public class EntryResource {
 	public ResponseEntity<Entry> searchById(@PathVariable Long id) {
 		Entry entry = entryRepository.findOne(id);
 		return entry != null ? ResponseEntity.ok(entry) : ResponseEntity.notFound().build();
+    }
+    
+    @PostMapping
+	public ResponseEntity<Entry> criar(@Valid @RequestBody Entry entry, HttpServletResponse response) {
+		Entry savedEntry = entryRepository.save(entry);
+		publisher.publishEvent(new CreatedResourceEvent(this, response, savedEntry.getId()));
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedEntry);
 	}
 	
 }
