d67eb73f5dc1eddd6388942bde7850a77ba56c77
Luis Filipe Cardoso <lfcamposcardoso@gmail.com>
2019-06-09 21:29:06 -0300

    entry delete method

--- src/main/java/com/luis/apiexample/resource/EntryResource.java
+++ src/main/java/com/luis/apiexample/resource/EntryResource.java
@@ -20,12 +20,14 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -69,5 +71,11 @@ public class EntryResource {
 		List<Error> errors = Arrays.asList(new Error(userMessage, devMessage));
 
 		return ResponseEntity.badRequest().body(errors);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remove(@PathVariable Long id) {
+		entryRepository.delete(id);
 	}
 }
