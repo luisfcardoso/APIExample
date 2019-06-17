f14fbbaf7425791321a10d32474473a9e76c1cc1
Luis Filipe Cardoso <lfcamposcardoso@gmail.com>
2019-06-16 21:37:49 -0300

    Update CorsFilter.java
    config cors

--- src/main/java/com/luis/apiexample/cors/CorsFilter.java
+++ src/main/java/com/luis/apiexample/cors/CorsFilter.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Component;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilter implements Filter {
 
-	private String allowedOrigin = "*"; 
+	private String allowedOrigin = "*"; // choose origin
 	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
